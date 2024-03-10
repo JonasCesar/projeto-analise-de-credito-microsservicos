941ca140a7716194098d4f3ceafda2de75e38566
Jonas César <jonascesar95@gmail.com>
2024-03-10 15:43:31 -0300

    notificacao - start do serviço de notificação

---
+++ notificacao/src/test/java/com/cesar/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
